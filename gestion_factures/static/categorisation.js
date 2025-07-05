c09c4460cca7d28528277f57d850114fcb2da9a5
er1 <94377537+eer1@users.noreply.github.com>
2025-07-05 12:06:49 +0200

    Base de données catégorisation OK

--- gestion_factures/static/categorisation.js
+++ gestion_factures/static/categorisation.js
@@ -2,14 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
   const table = document.getElementById('table-factures');
 
 
-  const noResultRow = document.createElement('tr');
-  const td = document.createElement('td');
-  td.colSpan = table.rows[0].cells.length;
-  td.style.textAlign = 'center';
-  td.textContent = 'Aucune facture';
-  noResultRow.appendChild(td);
-  noResultRow.style.display = 'none';
-  table.tBodies[0].appendChild(noResultRow);
 
  
     });
